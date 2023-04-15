c36b9074e610c5833a04bff56c16b828e4541314
Alekstitula84 <alekstitula84@gmail.com>
2023-04-15 12:16:52 +0300

    step1

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,74 +1,84 @@
-import { Component } from 'react';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 import PropTypes from 'prop-types';
 import { AddBtn, Form } from './ContactForm.styled';
 
-export class ContactForm extends Component {
-    static propTypes = {
-        add: PropTypes.func.isRequired,
-        contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
-    };
-    state = {
-        name: '',
-        number: '',
+export const ContactForm = ({ add, contacts }) => {
+    const [user, setUser] = useState({ name: '', number: '' });
+
+    const handleInputNameChange = e => {
+        setUser({ ...user, name: e.currentTarget.value });
     };
 
-    handleInputNameChange = e => {
-        this.setState({ name: e.currentTarget.value });
+    const handleInputTelChange = e => {
+        setUser({ ...user, number: e.currentTarget.value });
     };
 
-    handleInputTelChange = e => {
-        this.setState({ number: e.currentTarget.value });
-    };
-
-    handleFormSubmit = e => {
+    const handleFormSubmit = e => {
         e.preventDefault();
+        const ContactId = nanoid();
 
-        this.props.add({ ...this.state });
-        this.resetForm();
+        const contact = {
+            name: user.name,
+            id: ContactId,
+            number: user.number,
+        };
+
+        if (contacts.find(item => item.name === user.name)) {
+            alert(`${user.name} is already in contacts.`);
+            return;
+        } else if (contacts.find(item => item.number === user.number)) {
+            alert(`${user.number} is already in contacts.`);
+            return;
+        }
+
+        add(contact);
+        resetForm();
     };
 
-    resetForm = () => {
-        this.setState({
+    const resetForm = () => {
+        setUser({
             name: '',
             number: '',
         });
     };
 
-    render() {
-        return (
-            <>
-                <Form action="" onSubmit={this.handleFormSubmit}>
-                    <label htmlFor="name">
-                        <p>Name</p>
-                        <input
-                            type="text"
-                            name="name"
-                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                            required
-                            onChange={this.handleInputNameChange}
-                            value={this.state.name}
-                            placeholder='Petro Ivanovich'
-                        />
-                    </label>
-                    <label htmlFor="number">
-                        <p>Number</p>
-                        <input
-                            type="tel"
-                            name="number"
-                            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                            required
-                            onChange={this.handleInputTelChange}
-                            value={this.state.number}
-                            placeholder='+380971112233'
-                        />
-                    </label>
+    return (
+        <>
+            <Form action="" onSubmit={handleFormSubmit}>
+                <label htmlFor="name">
+                    <p>Name</p>
+                    <input
+                        type="text"
+                        name="name"
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        required
+                        onChange={handleInputNameChange}
+                        value={user.name}
+                    />
+                </label>
+                <label htmlFor="number">
+                    <p>Number</p>
+                    <input
+                        type="tel"
+                        name="number"
+                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        required
+                        onChange={handleInputTelChange}
+                        value={user.number}
+                    />
+                </label>
 
-                    <AddBtn type="submit">Add contact</AddBtn>
-                </Form>
-            </>
-        );
-    }
-}
+                <AddBtn type="submit">Add contact</AddBtn>
+            </Form>
+        </>
+    );
+};
+
+ContactForm.propTypes = {
+    add: PropTypes.func.isRequired,
+    contact: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
